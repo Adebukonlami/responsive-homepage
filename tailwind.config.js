fbda1edab7f7a23efd3445c298e8e6f1ee171d0b
adebukonla <kenzo4naps@gmail.com>
2021-11-24 22:45:01 +0000

    Add existing file

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const colors = require("tailwindcss/colors")
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Barlow'],
+        secondary: ['Fraunces']
+      },
+
+      colors: {
+        softRed: 'var(--soft-red)',
+        myYellow: 'var(--yellow)',
+        darkCyan: 'var(--dark-desaturated-cyan)',
+        darkBlue: 'var(--dark-blue)',
+        moderateCyan: 'var(--dark-moderate-cyan)',
+        darkDesaturatedBlue: 'var(--very-dark-desaturated-blue)',
+        vdarkGrayishBlue: 'var(--very-dark-grayish-blue)',
+        darkGrayishBlue: 'var(--dark-grayish-blue)',
+        grayishBlue: 'var(--grayish-blue)',
+        myWhite: 'var(--white)',
+      },
+
+      backgroundImage: {
+        hero: "url('/public/images/desktop/image-header.jpg')"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
